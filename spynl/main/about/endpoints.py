222bf6377242370971a9f11a31fe12fc743c3251
RosanneZe <RosanneZe@users.noreply.github.com>
2017-04-21 10:09:01 +0200

    SWPY-1085 made /about/versions available to all authenticated users and added scm-commit information. (#18)

--- spynl/main/about/endpoints.py
+++ spynl/main/about/endpoints.py
@@ -18,7 +18,7 @@ from spynl.main.dateutils import now, date_to_str
 from spynl.main.docs.settings import ini_doc, ini_description
 from spynl.main.docs.documentation import HIDE_TRYITOUT_IDS
 from spynl.main.pkg_utils import (get_spynl_packages,
-                                  lookup_scm_commit)
+                                  lookup_scm_commit, lookup_scm_commit_describe)
 
 
 def hello(request):
@@ -151,8 +151,10 @@ def versions(request):
         status    | string | 'ok' or 'error'\n
         spynl_version | string | The version of this Spynl instance.\n
         spynl_commit  | string | The git commit id this Spynl instance.\n
-        plugins   | dict   | {spynl-plugin: {commit: SCM commit id,
-        version: package version}} for each Spynl plugin.\n
+        plugins   | dict   | {spynl-plugin: {commit: SCM commit id of the HEAD,
+        version: package version, scmVersion: state of the working directory,
+        will show version and commit, and dirty if the working directory
+        contains uncommited changes}} for each Spynl plugin.\n
         time      | string | time in format: $setting[spynl.date_format]\n
     """
     response = {}
@@ -165,7 +167,8 @@ def versions(request):
     packages = get_spynl_packages()
     for package in packages:
         pinfo = dict(version=package.version,
-                     commit=lookup_scm_commit(package.location))
+                     commit=lookup_scm_commit(package.location),
+                     scmVersion=lookup_scm_commit_describe(package.location))
         response['plugins'][package.project_name] = pinfo
     return response
 
--- spynl/main/pkg_utils.py
+++ spynl/main/pkg_utils.py
@@ -88,9 +88,28 @@ def lookup_scm_commit(package_location):
         return cmd_result.stdout.strip()
 
 
+def lookup_scm_commit_describe(package_location):
+    """
+    Look up the SCM commit ID to give the version and, if the latest commit is
+    not that of the version tag, the commit number (for git, hg works slightly
+    differently)
+    """
+    with chdir(package_location):
+        if os.path.exists('.git'):
+            cmd = 'git describe --dirty'
+        elif os.path.exists('.hg'):
+            cmd = 'hg log -r . --template '\
+              '"{latesttag}-{latesttagdistance}-{node|short}\n"'
+        else:
+            return None
+        cmd_result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,
+                                    universal_newlines=True)
+        return cmd_result.stdout.strip()
+
+
 def get_config_package(require=None):
     """
-    Return the config package. Complain if several packages have the 
+    Return the config package. Complain if several packages have the
     .ini files.
     """
     config_package = None
