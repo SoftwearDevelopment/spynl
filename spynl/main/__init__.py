b3f12b48805504d9f849e27a4921bade96a31f22
Mohammed Kareem <kareeeeem@gmail.com>
2017-05-17 14:26:06 +0200

    SWPY-1095 plugins.py was needlessly complicated
    we were also not using setuptool features correctly.

--- spynl/main/__init__.py
+++ spynl/main/__init__.py
@@ -1,17 +1,25 @@
 """The main package of Spynl."""
 
-
-import os.path
+from pkg_resources import iter_entry_points  # pylint: disable=E0611
 
 from pyramid.config import Configurator
-from pyramid.settings import asbool
 from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.viewderivers import INGRESS
 
-from spynl.main import (plugins, serial, about,
-                        routing, events, endpoints, session)
-from spynl.main.utils import (renderer_factory, check_origin,
-                              handle_pre_flight_request)
+from spynl.main import (
+    serial,
+    about,
+    plugins,
+    routing,
+    events,
+    endpoints,
+    session,
+)
+from spynl.main.utils import (
+    renderer_factory,
+    check_origin,
+    handle_pre_flight_request,
+)
 from spynl.main.exceptions import SpynlException
 from spynl.main.error_views import spynl_error, error400, error500
 from spynl.main.validation import validate_json_schema
@@ -20,7 +28,6 @@ from spynl.main.docs.documentation import make_docs
 from spynl.main.docs.settings import check_required_settings
 from spynl.main.dateutils import now
 from spynl.main.locale import TemplateTranslations
-
 
 
 def main(global_config, test_settings=None, **settings):
@@ -41,8 +48,7 @@ def main(global_config, test_settings=None, **settings):
 
     # Add spynl.main's view derivers
     config.add_view_deriver(validate_json_schema)
-    config.add_view_deriver(handle_pre_flight_request,
-                            under=INGRESS) # do this first
+    config.add_view_deriver(handle_pre_flight_request, under=INGRESS)
     config.add_view_deriver(check_origin)
 
     # initialize the main plugins
@@ -85,8 +91,11 @@ def main(global_config, test_settings=None, **settings):
     # add jinja for templating
     config.include('pyramid_jinja2')
     config.add_settings({
-        'jinja2.filters': {'static_url': 'pyramid_jinja2.filters:static_url_filter'},
-        'jinja2.i18n.gettext': TemplateTranslations})
+        'jinja2.i18n.gettext': TemplateTranslations,
+        'jinja2.filters': {
+            'static_url': 'pyramid_jinja2.filters:static_url_filter'
+        },
+    })
 
     # initialise mailer, use dummy mailer for tests
     if test_settings:
@@ -96,6 +105,5 @@ def main(global_config, test_settings=None, **settings):
 
     config.commit()
     config.registry.notify(plugins.PluginsLoaded(config))
-
 
     return config.make_wsgi_app()
